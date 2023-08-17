bb754d32c7a6498481c230e95c3bc0df1c5e7b7c
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-08-17 12:14:38 +0600

    dynamic api integrated in allEvent and EventDetails page

--- src/Pages/Event/AllEventPage.js
+++ src/Pages/Event/AllEventPage.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useTitle from '../../hooks/useTitle';
-import HeroComponent1 from '../../Components/Common/HeroComponent1';
-import EventsGridCarousol from './EventGridCarousol';
+import Marquee from "react-fast-marquee";
+import { Link, useNavigate } from 'react-router-dom';
 import './AllEventPage.css'
+import { useContext } from 'react';
+import { AllContext } from '../../hooks/ContextData';
+import Loader from '../../Components/Common/Loader';
 
 const AllEventPage = () => {
     useTitle("Events")
+    const { loading, setLoading } = useContext(AllContext);
+    const [allEvents, setAllEvents] = useState([]);
+    // const { date, description, events, id, image, start_date, status, time, title, video_link } = allEvents;
+    const navigate = useNavigate();
+
+    console.log("allEvents : ", allEvents);
+
+    useEffect(() => {
+        fetch("http://dev.bpsa.com.bd/api/event")
+            .then(response => response.json())
+            .then(data => {
+                // console.log("All Event :", data.data.event)
+                setAllEvents(data.data.event);
+            })
+    }, [])
+
+
+
+    if (loading) {
+        <Loader></Loader>
+    }
+
     return (
         <div className='eventsArea'>
-            {/* <HeroComponent1 title="Events" /> */}
-            <h1 className=' fw-bold text-center text-success my-4'>All Events</h1>
-            <div >
-                <EventsGridCarousol></EventsGridCarousol>
+            <h1 className='fw-bold text-center text-success my-4'>All Events</h1>
+            <div className='d-flex col-md-10 mx-auto'>
+
+                <Marquee pauseOnHover={true} gradient={false}>
+                    {allEvents &&
+                        allEvents?.map((item, index) => {
+                            return (
+                                <div className=' AllEventCard'>
+                                    <div onClick={() => navigate(`/events/${item?.id}`)} key={index}>
+                                        <img src={item?.image} className='allEventImage' alt="" />
+                                        <div className=' text-center mt-2'>
+                                            <p className=' fw-bold my-0 text-primary'>{item?.title.slice(0, 27)}</p>
+                                            <p className=' my-0'><b>Event:</b> {item?.events}</p>
+                                            <p className=' my-0'><b>Date:</b> {item?.date}</p>
+                                            <p className=' my-0'>{item?.description.slice(0, 30)}</p>
+                                            <Link className='btn btn-outline-primary btn-sm w-100 ' to={`/events/${item?.id}`}>Show Details</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                </Marquee>
+
             </div>
         </div>
     );
