887731acf8be74fa3ac9774128f7886917a7adc8
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-09-06 11:32:15 +0600

    updated noticeline latest box color

--- src/Components/Common/NoticeLine.js
+++ src/Components/Common/NoticeLine.js
@@ -36,8 +36,8 @@ const NoticeLine = ({ data }) => {
   }
 
   return (
-    <div className="p-2 md:p-3 flex items-center space-x-2 md:space-x-4 bg-main">
-      <span className="text-white md:tracking-widest font-semibold text-xs md:text-[16px]">
+    <div className="p-2 md:p-3 flex items-center space-x-2 md:space-x-4 bg-main/80">
+      <span className=" bg-second py-3 px-2  mx-[-8px] my-[-8px] text-white md:tracking-widest font-semibold text-xs md:text-[16px]">
         LATEST
       </span>
       {/* <ImageComponent image={noticeImg} className="w-[140px] object-contain" /> */}
@@ -48,14 +48,12 @@ const NoticeLine = ({ data }) => {
               <div
                 onClick={() => navigate(`/news/${item?.id}`)}
                 key={index}
-                className={`bg-main flex items-center  cursor-pointer ${
-                  index % 2 === 0 ? "text-white" : "text-white"
-                }`}
+                className={` flex items-center  cursor-pointer ${index % 2 === 0 ? "text-white" : "text-white"}`}
               >
                 <span className="  text-white mx-4">
                   {/* <ArrowRightIcon /> */} |
-                </span>{" "}
-                {item?.Heading}{" "}
+                </span>
+                {item?.Heading}
               </div>
             );
           })}
